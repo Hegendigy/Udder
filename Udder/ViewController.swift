d940ed0c9e406c4a71f4462622e5e5c7b3e0e0df
Hendy Christian <hendy.christian001@gmail.com>
2020-08-04 11:26:06 +0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Hendy Christian on 04/08/20.
+//  Copyright © 2020 hendyProject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
